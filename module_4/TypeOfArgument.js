d5fe1e147c29ee41172359b49eae3b5e725f92bc
Lisov Pavel <lisoffpavel@gmail.com>
2022-05-29 16:56:38 +0300

    добавлена задача про тип аргумента

---
+++ module_4/TypeOfArgument.js
@@ -0,0 +1,10 @@
+function typeOfArgument(a){
+    let result = typeof a;
+    return console.log(result);
+};
+typeOfArgument(5);
+typeOfArgument();
+typeOfArgument('0');
+typeOfArgument(true);
+typeOfArgument(null);
+typeOfArgument(undefined);
