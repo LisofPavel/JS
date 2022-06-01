1a8788346f456e35a5cbb602ee3e4ee388b802a7
Lisov Pavel <lisoffpavel@gmail.com>
2022-06-01 20:09:21 +0300

    исправления

--- module_4/PrimeNumber.js
+++ module_4/PrimeNumber.js
@@ -1,16 +1,22 @@
 function prime(x){
 if (x>1){
-    for(let i=2;i<x;i++){
-        if (x%i == 0){
+    for(let i=2;i<=x;i++){
+        if (x%i == 0 && x!=i){
             return console.log('not prime');
-            
-        } else return console.log('is prime');
-    }
+            break
+        };
+        if (x%i==0 && i==x) {
+          return console.log('is prime');
+        };
+    };
 } else return console.log('not prime');
 };
 prime(-25);
 prime(0);
 prime(1);
 prime(4);
-prime(7);
+prime(35);
 prime(101);
+prime(7);
+prime(41);
+prime(49);
